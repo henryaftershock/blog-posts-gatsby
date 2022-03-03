1c9f1da60816816cd4e5b57d15ba34e4a24c2dcd
henryaftershock <henry@aftershock.agency>
2022-03-03 12:12:32 -0500

    Gatsby blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,6 @@
-exports.createPages = async ({ actions }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -6,4 +8,39 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: require.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,39 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 0.5rem;
+  color: #0070f3;
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
+    <div>
+      <h1>Henry's blog!</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => {
+        console.log(node)
+        return (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        )
+      })}
+    </div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -29,3 +55,26 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          html
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
